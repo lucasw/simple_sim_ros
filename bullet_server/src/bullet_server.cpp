c6c42139fc1f33015c69a806eb5bee395edb033c
Lucas Walter <wsacul@gmail.com>
2016-04-25 06:51:23 -0700

    Exact tutorial works, next bring in ros, then classify

--- bullet_server/src/bullet_server.cpp
+++ bullet_server/src/bullet_server.cpp
@@ -18,6 +18,7 @@
   gets more than 5 or 10 seconds behind current time).
 */
 
+#include <iostream>
 #include <bullet/btBulletDynamicsCommon.h>
 // #include <ros/ros.h>
 
@@ -110,5 +111,72 @@ BulletServer::BulletServer()
 int main(int argc, char** argv)
 {
   // ros::init(argc, argv, "bullet_server");
-  BulletServer bullet_server;
+  //  BulletServer bullet_server;
+
+
+  // exact hello world
+        btBroadphaseInterface* broadphase = new btDbvtBroadphase();
+
+        btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+        btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+
+        btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+
+        btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+
+        dynamicsWorld->setGravity(btVector3(0, -10, 0));
+
+
+        btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
+
+        btCollisionShape* fallShape = new btSphereShape(1);
+
+
+        btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
+        btRigidBody::btRigidBodyConstructionInfo
+                groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
+        btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
+        dynamicsWorld->addRigidBody(groundRigidBody);
+
+
+        btDefaultMotionState* fallMotionState =
+                new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
+        btScalar mass = 1;
+        btVector3 fallInertia(0, 0, 0);
+        fallShape->calculateLocalInertia(mass, fallInertia);
+        btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
+        btRigidBody* fallRigidBody = new btRigidBody(fallRigidBodyCI);
+        dynamicsWorld->addRigidBody(fallRigidBody);
+
+
+        for (int i = 0; i < 300; i++) {
+                dynamicsWorld->stepSimulation(1 / 60.f, 10);
+
+                btTransform trans;
+                fallRigidBody->getMotionState()->getWorldTransform(trans);
+
+                std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
+        }
+
+        dynamicsWorld->removeRigidBody(fallRigidBody);
+        delete fallRigidBody->getMotionState();
+        delete fallRigidBody;
+
+        dynamicsWorld->removeRigidBody(groundRigidBody);
+        delete groundRigidBody->getMotionState();
+        delete groundRigidBody;
+
+
+        delete fallShape;
+
+        delete groundShape;
+
+
+        delete dynamicsWorld;
+        delete solver;
+        delete collisionConfiguration;
+        delete dispatcher;
+        delete broadphase;
+
+        return 0;
 }

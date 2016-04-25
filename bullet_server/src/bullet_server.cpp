728fc065bb9708304003ff93801d8e2d88454928
Lucas Walter <wsacul@gmail.com>
2016-04-25 06:44:04 -0700

    This crashes at stepSimulation

--- bullet_server/src/bullet_server.cpp
+++ bullet_server/src/bullet_server.cpp
@@ -19,11 +19,11 @@
 */
 
 #include <bullet/btBulletDynamicsCommon.h>
-#include <ros/ros.h>
+// #include <ros/ros.h>
 
 class BulletServer
 {
-  ros::NodeHandle nh_;
+  // ros::NodeHandle nh_;
 
   // Build the broadphase
   btBroadphaseInterface* broadphase_;
@@ -37,27 +37,78 @@ class BulletServer
 
   // The world.
   btDiscreteDynamicsWorld* dynamics_world_;
+
 public:
   BulletServer();
 };
 
 BulletServer::BulletServer()
 {
+  // http://www.bulletphysics.org/mediawiki-1.5.8/index.php/Hello_World
   btBroadphaseInterface* broadphase = new btDbvtBroadphase();
 
   collision_configuration_ = new btDefaultCollisionConfiguration();
   dispatcher_ = new btCollisionDispatcher(collision_configuration_);
 
-  solver_ = new btSequentialImpulseConstraintSolver;
+  solver_ = new btSequentialImpulseConstraintSolver();
 
   dynamics_world_ = new btDiscreteDynamicsWorld(dispatcher_, broadphase_,
       solver_, collision_configuration_);
+
+  // TODO(lucasw) set this from param
   dynamics_world_->setGravity(btVector3(0, -10, 0));
+
+  #if 0
+  {
+    btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
+
+    btDefaultMotionState* groundMotionState =
+                    new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1),
+                                                         btVector3(0, -1, 0)));
+    btRigidBody::btRigidBodyConstructionInfo
+                     groundRigidBodyCI(0, groundMotionState, groundShape,
+                                       btVector3(0, 0, 0));
+    btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
+
+    dynamics_world_->addRigidBody(groundRigidBody);
+  }
+
+  btRigidBody* fallRigidBody;
+  {
+    btDefaultMotionState* fallMotionState =
+                new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
+    btCollisionShape* fallShape = new btSphereShape(1);
+    btScalar mass = 1;
+    btVector3 fallInertia(0, 0, 0);
+    fallShape->calculateLocalInertia(mass, fallInertia);
+
+    btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState,
+                                                             fallShape, fallInertia);
+    fallRigidBody = new btRigidBody(fallRigidBodyCI);
+    dynamics_world_->addRigidBody(fallRigidBody);
+  }
+  #endif
+
+  const float rate = 60.0;
+  const float period = 1.0 / rate;
+  int i = 0;
+  while (true)  // (ros::ok())
+  {
+    // float ms = getDeltaTimeMicroseconds();
+    // float period = ms / 1e6;
+    // ROS_INFO_STREAM(++i << " " << period << " " << dynamics_world_);
+    dynamics_world_->stepSimulation(period, 1, period*2);
+
+    btTransform trans;
+    // fallRigidBody->getMotionState()->getWorldTransform(trans);
+    // ROS_INFO_STREAM("sphere height: " << trans.getOrigin().getY());
+
+    // ros::spinOnce();
+  }
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "bullet_server");
+  // ros::init(argc, argv, "bullet_server");
   BulletServer bullet_server;
-  ros::spin();
 }

1d5a6c5f046aabc02b785696aea9fda5b7583bfc
Lucas Walter <wsacul@gmail.com>
2016-05-04 07:07:44 -0700

    Generate random joint

--- bullet_server/scripts/random_body.py
+++ bullet_server/scripts/random_body.py
@@ -19,14 +19,18 @@ impulse_pub = rospy.Publisher("add_impulse", Impulse, queue_size=1)
 sleep_time = 2.0
 postfix = str(random.randrange(0, 1000000000))
 count = 0
+
+bodies = {}
+
 while not rospy.is_shutdown():
     z_height = 8.8
     wheel = Body()
     wheel.name = "random_body_" + postfix + "_" + str(count)
+    bodies[count] = wheel.name
     wheel.type = random.randint(0, 4)  # Body.CYLINDER
     wheel.pose.position.x = random.uniform(-0.1, 0.1)
     wheel.pose.position.y = random.uniform(-0.1, 0.1)
-    wheel.pose.position.z = z_height + random.uniform(0, 2.0)
+    wheel.pose.position.z = z_height + random.uniform(0, 4.0)
     # TODO(lucasw) make a random rotation
     wheel.pose.orientation.x = 0.0  # 0.707
     wheel.pose.orientation.w = random.uniform(0.9, 1.0)  # 0.707
@@ -40,4 +44,20 @@ while not rospy.is_shutdown():
 
     # TODO(lucasw) create random joints between existing links
     # store the names of all the existing bodies
+    if False:  # random.random() > 0.5:
+        axel = Constraint()
+        axel.name = "constraint_" + postfix + "_" + str(count)
+        axel.type = Constraint.POINT2POINT
+        axel.body_a = bodies[random.randrange(0, count)]
+        axel.body_b = bodies[random.randrange(0, count)]
+        if (axel.body_a == axel.body_b):
+            continue
+        axel.pivot_in_a.x = 0.0  # wheel0.pose.position.x
+        axel.pivot_in_a.y = 2.0
+        axel.pivot_in_a.z = 0.0
+        axel.pivot_in_b.y = -2.0
+        constraint_pub.publish(axel)
+        rospy.loginfo(axel)
+        rospy.sleep(sleep_time)
+
 

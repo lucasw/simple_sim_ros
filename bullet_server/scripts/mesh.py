a2cc43d63ccdc8b49226b7caddd086dd1acda8c2
Lucas Walter <wsacul@gmail.com>
2016-05-06 22:33:48 -0700

    Beginning of support for soft body- the most generic type will be a trimesh one.  Later look at automatic conversion of tri-mesh to tetras with python tools, and create a tetra message type.

---
+++ bullet_server/scripts/mesh.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+
+rospy.init_node("stl")
+
+file_name = rospy.get_param("~file_name")
+
+if False:
+    import vtk
+    reader = vtk.vtkSTLReader()
+    reader.SetFileName(fil_name)
+    reader.Update()
+    polydata = reader.GetOutput()
+    print polydata.GetPoints()
+    print polydata.GetPolys()
+
+from pyassimp import pyassimp
+
+scene = pyassimp.load(file_name)
+print scene
+mesh = scene.meshes[0]
+
+print dir(mesh)
+print mesh.faces[0].indices
+# print mesh.vertices
+# print mesh.
+
+#TODO(lucasw) convert the vertices and indices to a shape_msg/Mesh

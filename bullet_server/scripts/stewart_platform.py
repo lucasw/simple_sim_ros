f3b681ebaa62007531feb6005407ce2de1922611
Lucas Walter <wsacul@gmail.com>
2016-10-09 17:05:46 -0700

    Trying to make bottom half cylinders, but they aren't being specified correctly and the server crashes if a same named joint is re-added (should be deleted properly like bodies are)

--- bullet_server/scripts/stewart_platform.py
+++ bullet_server/scripts/stewart_platform.py
@@ -27,7 +27,7 @@ class StewartPlatform:
         bot_plate.pose.orientation.y = rot90[1]
         bot_plate.pose.orientation.z = rot90[2]
         bot_plate.pose.orientation.w = rot90[3]
-        bot_plate.pose.position.z = floor + 0.1
+        bot_plate.pose.position.z = floor + 0.2
         bot_plate.type = Body.CYLINDER
         bot_plate.scale.x = radius
         bot_plate.scale.y = thickness
@@ -46,9 +46,41 @@ class StewartPlatform:
         top_plate.scale.x = radius
         top_plate.scale.y = thickness
         top_plate.scale.z = radius
-        add_compound_request.body.append(top_plate)
+        # add_compound_request.body.append(top_plate)
 
         # make six actuator cylinder bottoms with TBD spacing in a circle
+        for i in range(6):
+            bot_cylinder = Body()
+            bot_cylinder.name = "bot_cylinder_" + str(i)
+            bot_cylinder.pose.orientation.x = rot90[0]
+            bot_cylinder.pose.orientation.y = rot90[1]
+            bot_cylinder.pose.orientation.z = rot90[2]
+            bot_cylinder.pose.orientation.w = rot90[3]
+            bot_cylinder.pose.position.x = 0.7 * radius * math.cos(i/6.0 * 2.0 * math.pi)
+            bot_cylinder.pose.position.y = 0.7 * radius * math.sin(i/6.0 * 2.0 * math.pi)
+            bot_cylinder.pose.position.z = floor + 0.2 + height/6.0 * 0.5 + 0.03
+            bot_cylinder.type = Body.CYLINDER
+            bot_cylinder.scale.x = thickness/2.0
+            bot_cylinder.scale.y = height/8.0
+            bot_cylinder.scale.z = thickness/2.0
+            add_compound_request.body.append(bot_cylinder)
+
+            # This will crash the server if run twice-
+            # having a new constraint with same name as old one isn't handled
+            # correctly, but it isn't removing old one properly.
+            constraint = Constraint()
+            constraint.name = "bot_cylinder_p2p_" + str(i)
+            constraint.body_a = "bottom_plate"
+            constraint.body_b = bot_cylinder.name
+            constraint.type = Constraint.POINT2POINT
+            constraint.pivot_in_a.x = bot_cylinder.pose.position.x
+            constraint.pivot_in_a.y = bot_cylinder.pose.position.y
+            constraint.pivot_in_a.z = bot_cylinder.pose.position.z + 0.2
+            constraint.pivot_in_b.x = 0
+            constraint.pivot_in_b.y = 0
+            constraint.pivot_in_b.z = -bot_cylinder.scale.y - 0.05
+
+            add_compound_request.constraint.append(constraint)
 
         # connect the cylinders to the bottom plate with p2p ball socket
         # joints.

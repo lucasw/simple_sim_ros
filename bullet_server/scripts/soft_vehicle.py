d65cb528283c91b165ca8391cc3b260b4478e8c9
Lucas Walter <wsacul@gmail.com>
2016-11-20 04:43:02 -0800

    Now have a four wheeled vehicle, but the wheels slide a lot- increase friction through the Config

--- bullet_server/scripts/soft_vehicle.py
+++ bullet_server/scripts/soft_vehicle.py
@@ -9,7 +9,8 @@ import tf
 from bullet_server.msg import Anchor, Body, Constraint, Face, Link, Material, Node, SoftBody, Tetra
 from bullet_server.srv import *
 
-def make_rigid_box(name, mass, xs, ys, zs, wd, ln, ht):
+def make_rigid_box(name, mass, xs, ys, zs, wd, ln, ht,
+                   roll=0, pitch=0, yaw=0):
     body = Body()
     body.name = name
     body.type = Body.BOX
@@ -17,7 +18,12 @@ def make_rigid_box(name, mass, xs, ys, zs, wd, ln, ht):
     body.pose.position.x = xs
     body.pose.position.y = ys
     body.pose.position.z = zs
-    body.pose.orientation.w = 1.0
+    rot90 = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+    body.pose.orientation.x = rot90[0]
+    body.pose.orientation.y = rot90[1]
+    body.pose.orientation.z = rot90[2]
+    body.pose.orientation.w = rot90[3]
+
     body.scale.x = wd
     body.scale.y = ln
     body.scale.z = ht
@@ -44,7 +50,7 @@ def make_rigid_cylinder(name, mass, xs, ys, zs, radius, thickness,
     return body
 
 def make_soft_cube(name, node_mass, xs, ys, zs, ln,
-                   nx=4, ny=4, nz=4):
+                   nx=4, ny=4, nz=4, flip=1.0):
     body = SoftBody()
     body.name = name
 
@@ -53,9 +59,9 @@ def make_soft_cube(name, node_mass, xs, ys, zs, ln,
             for k in range(nz):
                 n1 = Node()
                 n1.mass = node_mass
-                n1.position.x = xs + (i - nx/2 + 0.5) * ln
-                n1.position.y = ys + (j - ny/2 + 0.5) * ln
-                n1.position.z = zs + (k - nz/2 + 0.5) * ln
+                n1.position.x = xs + (i - nx/2 + 0.5) * ln * flip
+                n1.position.y = ys + (j - ny/2 + 0.5) * ln * flip
+                n1.position.z = zs + (k - nz/2 + 0.5) * ln * flip
                 body.node.append(n1)
 
     for ind1 in range(len(body.node)):
@@ -72,12 +78,82 @@ def make_soft_cube(name, node_mass, xs, ys, zs, ln,
                         body.link.append(l1)
 
     mat = Material()
-    mat.kLST = 0.2
+    mat.kLST = 0.25
     mat.kVST = 0.1
     mat.kAST = 0.1
     body.material.append(mat)
 
     return body
+
+def make_wheel_assembly(prefix, xs, ys, zs, flip=1.0):
+    motor_mass = 0.2
+    motor_thickness = 0.1
+    motor_radius = 0.2
+    motor_y = 0.8
+    motor = make_rigid_cylinder(prefix + "_motor", motor_mass,
+                                xs, ys - motor_y * flip, zs,
+                                motor_radius, motor_thickness,
+                                0, 0, 0)
+
+    hinge = Constraint()
+    hinge.name = prefix + "_motor_hinge"
+    hinge.body_a = "chassis"
+    hinge.body_b = prefix + "_motor"
+    hinge.type = Constraint.HINGE
+    hinge.pivot_in_a.x = xs
+    hinge.pivot_in_a.y = -motor_y * flip
+    hinge.pivot_in_a.z = 0
+    hinge.pivot_in_b.x = 0
+    hinge.pivot_in_b.y = 0
+    hinge.pivot_in_b.z = 0
+    hinge.axis_in_a.x = 0
+    hinge.axis_in_a.y = 1.0
+    hinge.axis_in_a.z = 0
+    hinge.axis_in_b.x = 0
+    hinge.axis_in_b.y = 1.0
+    hinge.axis_in_b.z = 0
+    # These have to be < -pi and > pi to be unlimited
+    hinge.lower_ang_lim = -3.2  # -math.pi
+    hinge.upper_ang_lim = 3.2  # math.pi
+    hinge.max_motor_impulse = 25000.0
+
+    wheel_offset = 1.5
+    nx = 4
+    ny = 4
+    nz = 4
+    soft_length = 0.4
+    node_mass = 0.1
+    wheel = make_soft_cube(prefix + "wheel", node_mass,
+                           xs, ys - wheel_offset * flip, zs, soft_length,
+                           nx, ny, nz,
+                           flip)
+
+    # attach the rigid motor wheel to the soft wheel
+    for i in range(2):
+        for j in range(3):
+            for k in range(2):
+                xi = i + 1  # int(nx/2) - 1
+                yi = j + 1  # int(ny/2) - 1
+                zi = k + 1  # int(nz/2) - 1
+                ind = xi * ny * nz + yi * nz + zi
+                if ind < len(wheel.node):
+                    anchor = Anchor()
+                    anchor.disable_collision_between_linked_bodies = False
+                    # the weaker the influence, the longer the spring between
+                    # the local_pivot and the node
+                    # TODO(lucasw) probably should have more anchors,
+                    # but weaken the influence with distance from the wheel
+                    # axle
+                    anchor.influence = 1.0
+
+                    anchor.node_index = ind
+                    anchor.rigid_body_name = prefix + "_motor"
+                    anchor.local_pivot.x = wheel.node[ind].position.x - xs
+                    anchor.local_pivot.y = wheel.node[ind].position.y - ys + 0.8 * flip
+                    anchor.local_pivot.z = wheel.node[ind].position.z - zs
+                    wheel.anchor.append(anchor)
+
+    return (motor, hinge, wheel)
 
 class SoftVehicle:
     def __init__(self):
@@ -96,116 +172,57 @@ class SoftVehicle:
             ground_thickness = 1.0
             ground_mass = 0.0
             ground = make_rigid_cylinder("ground", ground_mass,
-                                         0, 0, -ground_thickness, ground_radius,
+                                         0, 0, -ground_thickness,
+                                         ground_radius,
                                          ground_thickness,
                                          math.pi/2.0, 0, 0)
             add_compound_request.body.append(ground)
 
-        chassis = make_rigid_box("chassis", 1.5, xs, ys, zs, 1.2, 0.5, 0.3)
+            # obstacles
+            obstacle = make_rigid_cylinder("obstacle1", ground_mass,
+                                           -6.0, 1.0, -ground_thickness + 0.5,
+                                           3.0,
+                                           ground_thickness,
+                                           math.pi/2.0, 0, 0)
+            add_compound_request.body.append(obstacle)
+            obstacle = make_rigid_box("obstacle2", ground_mass,
+                                      -12.0, 1.5, 0.5,
+                                      3.0, 1.0, 0.5,
+                                      0.1, 0.05, 0.01)
+            add_compound_request.body.append(obstacle)
+
+
+        chassis = make_rigid_box("chassis", 1.5, xs, ys, zs, 1.8, 0.5, 0.3)
         add_compound_request.body.append(chassis)
 
-        motor_mass = 0.2
-        motor_thickness = 0.1
-        motor_radius = 0.2
         motor_y = 0.8
-        left_motor = make_rigid_cylinder("left_motor", motor_mass,
-                                         xs, ys - motor_y, zs, motor_radius, motor_thickness,
-                                         0, 0, 0)
-        add_compound_request.body.append(left_motor)
-        right_motor = make_rigid_cylinder("right_motor", 0.3,
-                                          xs, ys + motor_y, zs, motor_radius, motor_thickness,
-                                          0, 0, 0)
-        add_compound_request.body.append(right_motor)
 
-        hinge = Constraint()
-        hinge.name = "left_motor_hinge"
-        hinge.body_a = "chassis"
-        hinge.body_b = "left_motor"
-        hinge.type = Constraint.HINGE
-        hinge.pivot_in_a.x = 0
-        hinge.pivot_in_a.y = -motor_y
-        hinge.pivot_in_a.z = 0
-        hinge.pivot_in_b.x = 0
-        hinge.pivot_in_b.y = 0
-        hinge.pivot_in_b.z = 0
-        hinge.axis_in_a.x = 0
-        hinge.axis_in_a.y = 1.0
-        hinge.axis_in_a.z = 0
-        hinge.axis_in_b.x = 0
-        hinge.axis_in_b.y = 1.0
-        hinge.axis_in_b.z = 0
-        # These have to be < -pi and > pi to be unlimited
-        hinge.lower_ang_lim = -3.2  # -math.pi
-        hinge.upper_ang_lim = 3.2  # math.pi
-        hinge.max_motor_impulse = 25000.0
+        (motor, hinge, wheel) = make_wheel_assembly("front_left",
+                                                    xs - 1.5, ys - motor_y, zs, flip=1.0)
+        add_compound_request.body.append(motor)
         add_compound_request.constraint.append(hinge)
+        add_compound_request.soft_body.append(wheel)
 
-        hinge2 = copy.deepcopy(hinge)
-        hinge2.name = "right_motor_hinge"
-        hinge2.body_b = "right_motor"
-        hinge2.pivot_in_a.y = motor_y
-        add_compound_request.constraint.append(hinge2)
+        (motor, hinge, wheel) = make_wheel_assembly("back_left",
+                                                    xs + 1.5, ys - motor_y, zs, flip=1.0)
+        add_compound_request.body.append(motor)
+        add_compound_request.constraint.append(hinge)
+        add_compound_request.soft_body.append(wheel)
 
-        wheel_offset = 1.5
-        nx = 4
-        ny = 4
-        nz = 4
-        soft_length = 0.4
-        node_mass = 0.1
-        left_wheel = make_soft_cube("left_wheel", node_mass, xs, ys - wheel_offset, zs, soft_length,
-                                    nx, ny, nz)
+        (motor, hinge, wheel) = make_wheel_assembly("front_right",
+                                                    xs - 1.5, ys + motor_y, zs,
+                                                    flip=-1.0)
+        add_compound_request.body.append(motor)
+        add_compound_request.constraint.append(hinge)
+        add_compound_request.soft_body.append(wheel)
 
-        right_wheel = make_soft_cube("right_wheel", node_mass, xs, ys + wheel_offset, zs, soft_length,
-                                     nx, ny, nz)
-        # attach the rigid motor wheel to the soft wheel
-        if True:
-            for i in range(2):
-                for j in range(3):
-                    for k in range(2):
-                        anchor = Anchor()
-                        anchor.disable_collision_between_linked_bodies = False
-                        anchor.influence = 1.0
+        (motor, hinge, wheel) = make_wheel_assembly("back_right",
+                                                    xs + 1.5, ys + motor_y, zs,
+                                                    flip=-1.0)
+        add_compound_request.body.append(motor)
+        add_compound_request.constraint.append(hinge)
+        add_compound_request.soft_body.append(wheel)
 
-                        xi = i + 1  # int(nx/2)
-                        yi = j + 1  # + int(ny/2)
-                        zi = k + 1  # int(nz/2)
-                        ind = xi * ny * nz + yi * nz + zi
-                        # the weaker the influence, the longer the spring between
-                        # the local_pivot and the node
-                        if ind < len(left_wheel.node):
-                            anchor.node_index = ind
-                            anchor.rigid_body_name = "left_motor"
-                            anchor.local_pivot.x = left_wheel.node[ind].position.x - xs  # left_motor.pose.position.x
-                            anchor.local_pivot.y = left_wheel.node[ind].position.y - ys + 0.8  # left_motor.pose.position.y
-                            anchor.local_pivot.z = left_wheel.node[ind].position.z - zs  # left_motor.pose.position.z
-                            left_wheel.anchor.append(anchor)
-
-                        anchor = copy.deepcopy(anchor)
-
-                        xi = i + 1  # int(nx/2)
-                        yi = j  # + int(ny/2)
-                        zi = k + 1  # int(nz/2)
-                        ind = xi * ny * nz + yi * nz + zi
-                        if ind >= len(left_wheel.node):
-                            continue
-
-                        anchor.node_index = ind
-                        anchor.rigid_body_name = "right_motor"
-                        anchor.local_pivot.x = right_wheel.node[ind].position.x - xs  # left_motor.pose.position.x
-                        anchor.local_pivot.y = right_wheel.node[ind].position.y - ys - 0.8  # left_motor.pose.position.y
-                        anchor.local_pivot.z = right_wheel.node[ind].position.z - zs  # left_motor.pose.position.z
-                        print anchor.local_pivot
-                        anchor.disable_collision_between_linked_bodies = False
-                        # the weaker the influence, the longer the spring between
-                        # the local_pivot and the node
-                        anchor.influence = 1.0
-
-                        right_wheel.anchor.append(anchor)
-
-
-        add_compound_request.soft_body.append(left_wheel)
-
-        add_compound_request.soft_body.append(right_wheel)
 
         try:
             add_compound_response = self.add_compound(add_compound_request)

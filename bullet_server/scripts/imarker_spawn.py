d77794da22e607b08ba5190f0e913a9c957c90e9
Lucas Walter <wsacul@gmail.com>
2016-11-24 07:59:31 -0800

    Now can spawn bodies from the interactive marker

--- bullet_server/scripts/imarker_spawn.py
+++ bullet_server/scripts/imarker_spawn.py
@@ -10,18 +10,56 @@
 # Later being able to create joints between bodies would be nice.
 
 import rospy
+import tf
 
+from bullet_server.msg import Body, Face, Link, Material, Node, SoftBody, SoftConfig, Tetra
+from bullet_server.srv import *
 from interactive_markers.interactive_marker_server import *
+from interactive_markers.menu_handler import *
 from visualization_msgs.msg import *
 
 
 class InteractiveMarkerSpawn:
     def __init__(self):
+        rospy.wait_for_service('add_compound')
+        self.add_compound = rospy.ServiceProxy('add_compound', AddCompound)
+
+        # Ground
+        if True:
+            add_compound_request = AddCompoundRequest()
+            add_compound_request.remove = False
+            # make the top cylinder plate
+            ground = Body()
+            ground.name = "ground"
+            ground.mass = 0.0
+            rot90 = tf.transformations.quaternion_from_euler(0, 0, 0)
+            radius = 50
+            thickness = 1.0
+            ground.pose.orientation.x = rot90[0]
+            ground.pose.orientation.y = rot90[1]
+            ground.pose.orientation.z = rot90[2]
+            ground.pose.orientation.w = rot90[3]
+            ground.pose.position.z = -thickness
+            ground.type = Body.BOX
+            ground.scale.x = radius
+            ground.scale.y = radius
+            ground.scale.z = thickness
+            add_compound_request.body.append(ground)
+
+	    try:
+		add_compound_response = self.add_compound(add_compound_request)
+		rospy.loginfo(add_compound_response)
+	    except rospy.service.ServiceException as e:
+		rospy.logerr(e)
+
+	self.count = 0
         self.server = InteractiveMarkerServer("body_spawn")
         self.im = InteractiveMarker()
         self.im.header.frame_id = "map"
         self.im.name = "body_spawner"
         self.im.description = "Spawn a new body"
+
+        self.menu_handler = MenuHandler()
 
         self.marker = Marker()
         # TODO(lucasw) make a drop-down that can change this
@@ -42,6 +80,9 @@ class InteractiveMarkerSpawn:
         self.move_3d.markers.append(self.marker)
         self.im.controls.append(self.move_3d)
 
+        # TODO(lucasw) click and drag resizing will be best served
+        # by and interactive marker child that has a frame tf defined
+        # by the parent.
         self.resize_x = InteractiveMarkerControl()
         self.resize_x.interaction_mode = InteractiveMarkerControl.BUTTON
         self.resize_x.name = "resize_x"
@@ -58,11 +99,23 @@ class InteractiveMarkerSpawn:
         self.resize_x.markers.append(arrow)
         self.im.controls.append(self.resize_x)
 
+        menu = InteractiveMarkerControl()
+        menu.interaction_mode = InteractiveMarkerControl.MENU
+        menu.description = "Spawn"
+        menu.name = "spawn_menu"
+        self.im.controls.append(menu)
+
+        self.menu_handler.insert("Spawn", callback=self.process_feedback)
+        # TODO(lucasw) have a submenu that is updated with a list
+        # of all tf frames, and selecting one will cause that tf to
+        # be the parent of this marker.
+
         self.server.insert(self.im, self.process_feedback)
         self.server.applyChanges()
+        # TODO(lucasw) what does this do?
+        self.menu_handler.apply(self.server, self.im.name)
 
     def process_feedback(self, feedback):
-        # print feedback
         if feedback.control_name == "resize_x":
             if feedback.event_type == InteractiveMarkerFeedback.MOUSE_DOWN:
                 scale_x = self.im.controls[0].markers[0].scale.x * 1.2
@@ -72,6 +125,35 @@ class InteractiveMarkerSpawn:
                 # print "arrow button ", scale_x
                 self.server.insert(self.im, self.process_feedback)
                 self.server.applyChanges()
+        elif feedback.event_type == InteractiveMarkerFeedback.MENU_SELECT:
+        # feedback.control_name == "spawn_menu":
+            if feedback.menu_entry_id == 1:
+		self.count += 1
+		rospy.loginfo(feedback)
+		body = Body()
+		body.name = "imarker_spawned_body_" + str(self.count)
+		body.mass = 1.0
+		body.pose.position.x = feedback.pose.position.x
+		body.pose.position.y = feedback.pose.position.y
+		body.pose.position.z = feedback.pose.position.z
+		body.pose.orientation.x = feedback.pose.orientation.x
+		body.pose.orientation.y = feedback.pose.orientation.y
+		body.pose.orientation.z = feedback.pose.orientation.z
+		body.pose.orientation.w = feedback.pose.orientation.w
+		body.type = Body.BOX
+		body.scale.x = self.im.controls[0].markers[0].scale.x / 2.0
+		body.scale.y = self.im.controls[0].markers[0].scale.y / 2.0
+		body.scale.z = self.im.controls[0].markers[0].scale.z / 2.0
+
+		add_compound_request = AddCompoundRequest()
+		add_compound_request.remove = False
+		add_compound_request.body.append(body)
+
+		try:
+		    add_compound_response = self.add_compound(add_compound_request)
+		    rospy.loginfo(add_compound_response)
+		except rospy.service.ServiceException as e:
+		    rospy.logerr(e)
 
 if __name__ == '__main__':
     rospy.init_node('imarker_spawn')

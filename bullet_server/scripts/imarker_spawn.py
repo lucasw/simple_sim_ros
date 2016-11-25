660e66461f56c23d8659297f2468e8bc127875ab
Lucas Walter <wsacul@gmail.com>
2016-11-25 08:30:04 -0800

    Having trouble making the box resize and track the position properly- because the InteractiveMarkerControl has no position exposed, but internally there is a position that the MOVE_3D is changing, it is tricky to update the scale of the marker without changing the position of the marker also.  This suggests decoupling the position from the box- use an rviz_interactive_tf to position the box and have the box not translate relative to that tf, only resize.

--- bullet_server/scripts/imarker_spawn.py
+++ bullet_server/scripts/imarker_spawn.py
@@ -11,9 +11,11 @@
 
 import rospy
 import tf
+import tf2_ros
 
 from bullet_server.msg import Body, Face, Link, Material, Node, SoftBody, SoftConfig, Tetra
 from bullet_server.srv import *
+from geometry_msgs.msg import TransformStamped
 from interactive_markers.interactive_marker_server import *
 from interactive_markers.menu_handler import *
 from visualization_msgs.msg import *
@@ -21,6 +23,8 @@ from visualization_msgs.msg import *
 
 class InteractiveMarkerSpawn:
     def __init__(self):
+        self.br = tf2_ros.TransformBroadcaster()
+
         rospy.wait_for_service('add_compound')
         self.add_compound = rospy.ServiceProxy('add_compound', AddCompound)
 
@@ -52,6 +56,15 @@ class InteractiveMarkerSpawn:
 	    except rospy.service.ServiceException as e:
 		rospy.logerr(e)
 
+        self.spawn_frame = rospy.get_param("~frame", "spawn_frame")
+        self.ts = TransformStamped()
+        self.ts.header.frame_id = "map"
+        self.ts.child_frame_id = self.spawn_frame
+        self.ts.transform.rotation.w = 1.0
+        # self.just_resized = False
+        self.first_resize = True
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.update)
+
 	self.count = 0
         self.server = InteractiveMarkerServer("body_spawn")
         self.im = InteractiveMarker()
@@ -64,8 +77,8 @@ class InteractiveMarkerSpawn:
         self.marker = Marker()
         # TODO(lucasw) make a drop-down that can change this
         self.marker.type = Marker.CUBE
-        scale_x = 0.5
-        self.marker.scale.x = scale_x
+        self.scale_x = 0.5
+        self.marker.scale.x = self.scale_x
         self.marker.scale.y = 0.5
         self.marker.scale.z = 0.5
         self.marker.color.r = 0.5
@@ -83,21 +96,6 @@ class InteractiveMarkerSpawn:
         # TODO(lucasw) click and drag resizing will be best served
         # by and interactive marker child that has a frame tf defined
         # by the parent.
-        self.resize_x = InteractiveMarkerControl()
-        self.resize_x.interaction_mode = InteractiveMarkerControl.BUTTON
-        self.resize_x.name = "resize_x"
-        arrow = Marker()
-        arrow.type = Marker.ARROW
-        arrow.pose.position.x = scale_x * 0.5
-        arrow.scale.x = 0.8
-        arrow.scale.y = 0.3
-        arrow.scale.z = 0.3
-        arrow.color.r = 0.1
-        arrow.color.g = 0.9
-        arrow.color.b = 0.15
-        arrow.color.a = 1.0
-        self.resize_x.markers.append(arrow)
-        self.im.controls.append(self.resize_x)
 
         menu = InteractiveMarkerControl()
         menu.interaction_mode = InteractiveMarkerControl.MENU
@@ -115,18 +113,86 @@ class InteractiveMarkerSpawn:
         # TODO(lucasw) what does this do?
         self.menu_handler.apply(self.server, self.im.name)
 
-    def process_feedback(self, feedback):
+        #
+        self.resize_x_server = InteractiveMarkerServer("body_spawn/resize_x")
+
+        self.resize_x_im = InteractiveMarker()
+        self.resize_x_im.header.frame_id = self.spawn_frame
+        self.resize_x_im.name = "body_spawner_resize_x"
+        self.resize_x_im.description = "resize x of new body"
+
+        self.resize_x = InteractiveMarkerControl()
+        self.resize_x.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
+        self.resize_x.name = "resize_x"
+
+        arrow = Marker()
+        arrow.type = Marker.ARROW
+        arrow.pose.position.x = self.scale_x * 0.5
+        arrow.scale.x = 0.8
+        arrow.scale.y = 0.3
+        arrow.scale.z = 0.3
+        arrow.color.r = 0.1
+        arrow.color.g = 0.9
+        arrow.color.b = 0.15
+        arrow.color.a = 1.0
+        self.resize_x.markers.append(arrow)
+
+        self.resize_x_im.controls.append(self.resize_x)
+        self.resize_x_server.insert(self.resize_x_im, self.process_resize_x_feedback)
+        self.resize_x_server.applyChanges()
+
+    def process_resize_x_feedback(self, feedback):
         if feedback.control_name == "resize_x":
-            if feedback.event_type == InteractiveMarkerFeedback.MOUSE_DOWN:
-                scale_x = self.im.controls[0].markers[0].scale.x * 1.2
+            # if feedback.event_type == InteractiveMarkerFeedback.MOUSE_DOWN:
+            print feedback
+            if True:
+                self.scale_x = 2.0 * abs(feedback.pose.position.x)
                 # TODO(lucasw) make dict to get rid of hardcoding
-                self.im.controls[0].markers[0].scale.x = scale_x
-                self.im.controls[1].markers[0].pose.position.x = scale_x * 0.5
-                # print "arrow button ", scale_x
+                # the move_3d box marker
+                # Have to set the marker to where it already is, otherwise
+                # the initial values will overwrite it.
+                # But only do it first time
+                if self.first_resize:
+                    self.first_resize = False
+                    # if self.im.controls[0].markers[0].pose.position.x == 0 and
+                    #         self.im.controls[0].markers[0].pose.position.y == 0
+                    #         self.im.controls[0].markers[0].pose.position.z == 0:
+                    self.im.controls[0].markers[0].pose.position.x = self.ts.transform.translation.x
+                    self.im.controls[0].markers[0].pose.position.y = self.ts.transform.translation.y
+                    self.im.controls[0].markers[0].pose.position.z = self.ts.transform.translation.z
+                self.im.controls[0].markers[0].scale.x = self.scale_x
                 self.server.insert(self.im, self.process_feedback)
                 self.server.applyChanges()
-        elif feedback.event_type == InteractiveMarkerFeedback.MENU_SELECT:
-        # feedback.control_name == "spawn_menu":
+                # self.just_resized = True
+
+    def update(self, event):
+        self.br.sendTransform(self.ts)
+        # self.just_tf_updated = True
+        # print feedback
+        # if self.just_resized:
+        #     self.just_resized = False
+         
+    def process_feedback(self, feedback):
+        # TODO(lucasw) or use an rviz_interactive_marker to generate this tf?
+        self.ts.header.stamp = feedback.header.stamp
+        self.ts.transform.translation.x = feedback.pose.position.x
+        self.ts.transform.translation.y = feedback.pose.position.y
+        self.ts.transform.translation.z = feedback.pose.position.z
+        self.ts.transform.rotation.x = feedback.pose.orientation.x
+        self.ts.transform.rotation.y = feedback.pose.orientation.y
+        self.ts.transform.rotation.z = feedback.pose.orientation.z
+        self.ts.transform.rotation.w = feedback.pose.orientation.w
+
+        self.first_resize = True
+        if False:
+            self.im.controls[0].markers[0].pose.position.x = 0
+            self.im.controls[0].markers[0].pose.position.y = 0
+            self.im.controls[0].markers[0].pose.position.z = 0
+            self.server.insert(self.im, self.process_feedback)
+            self.server.applyChanges()
+
+        if feedback.event_type == InteractiveMarkerFeedback.MENU_SELECT:
+            # feedback.control_name == "spawn_menu":
             if feedback.menu_entry_id == 1:
 		self.count += 1
 		rospy.loginfo(feedback)

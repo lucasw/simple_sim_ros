e6ea1cd3e171a5a6663621e07e51a7a50172f983
Lucas Walter <wsacul@gmail.com>
2016-12-12 10:52:47 -0800

    #7 spawn a single track, next position the tracks better and put wheels inside them.

---
+++ bullet_server/scripts/tracks.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# Copyright (c) 2016 Lucas Walter
+# Make a tank-like tracked vehicle
+# This may require some 'cheats' to work and be computationally modest.
+
+# First pass:
+# Create a bunch of flat rigid body plates, connect them in a loop
+# with single axis rotation joints.
+# Have a drive wheel and roller wheels inside the loop.
+# Perhaps put ridges on the drive wheel and gaps between the tread plates,
+# and see if the tracks stay on.
+# Then add additional rigid bodies to try to keep the tracks on and
+# allow them to be propelled forward.
+
+import copy
+import math
+import rospy
+import tf
+
+from bullet_server.msg import Body, Constraint, Heightfield, Impulse
+from bullet_server.srv import *
+
+
+class TrackedVehicle:
+    def __init__(self):
+        rospy.wait_for_service('add_compound')
+        self.add_compound = rospy.ServiceProxy('add_compound', AddCompound)
+        add_compound_request = AddCompoundRequest()
+        add_compound_request.remove = rospy.get_param('~remove', False)
+
+	num_tracks = rospy.get_param("~num_tracks", 30)
+	track_width = rospy.get_param("~track_width", 0.1)
+	track_length = rospy.get_param("~track_width", 0.04)
+	track_height = rospy.get_param("~track_height", 0.01)
+	track_mass = rospy.get_param("~track_mass", 0.1)
+
+        gap = rospy.get_param("~gap", 0.03)
+        # spawn in a circle for now
+        radius = (track_width + gap) * num_tracks / (2.0 * math.pi)
+
+	track = Body()
+        track.type = Body.BOX
+	track.mass = track_mass
+	track.scale.x = track_length
+	track.scale.y = track_width
+        track.scale.z = track_height
+
+        for i in range(num_tracks):
+            trk = copy.deepcopy(track)
+            trk.name = "track_" + str(i)
+            fr = float(i) / float(num_tracks)
+            angle = 2.0 * math.pi * fr
+            rot = tf.transformations.quaternion_from_euler(0, -angle + math.pi/2.0, 0)
+            trk.pose.orientation.x = rot[0]
+            trk.pose.orientation.y = rot[1]
+            trk.pose.orientation.z = rot[2]
+            trk.pose.orientation.w = rot[3]
+
+	    trk.pose.position.x = radius * math.cos(angle)
+            trk.pose.position.z = 1.2 * radius + radius * math.sin(angle)
+            add_compound_request.body.append(trk)
+
+        # add hinges between tracks
+        for i in range(num_tracks):
+            constraint = Constraint()
+            constraint.name = "hinge_" + str(i)
+            constraint.body_a = "track_" + str(i)
+            constraint.body_b = "track_" + str((i + 1) % num_tracks)
+            constraint.type = Constraint.HINGE
+            constraint.pivot_in_a.x = -(track_length/2.0 + gap)
+            constraint.pivot_in_b.x = (track_length/2.0 + gap)
+            constraint.axis_in_a.y = 1.0
+            constraint.axis_in_b.y = 1.0
+            add_compound_request.constraint.append(constraint)
+
+        try:
+            add_compound_response = self.add_compound(add_compound_request)
+            rospy.loginfo(add_compound_response)
+        except rospy.service.ServiceException as e:
+            rospy.logerr(e)
+
+if __name__ == '__main__':
+    rospy.init_node('tracked_vehicle')
+    tracked_vehicle = TrackedVehicle()

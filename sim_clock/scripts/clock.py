78a7f12970ab07ab6e57780879f9fa45d42a496c
Lucas Walter <wsacul@gmail.com>
2017-09-16 20:07:40 -0700

    Now can move the sim clock forward either from dr or from a Float32 message, but there needs to be a way to accumulate so that step time could be smaller than dt time but sufficient calls to it will eventually result in a dt update.  The idea is that the bullet sim could publish a message saying it has advanced by a certain amount of time, then the sim clock would update by that amount of time, and they would march forward.  If real time factor were set very high then the bullet sim would run as fast as possible (using near 100% cpu) but the sim clock would run no faster, just keeping in step - haven't tested that though.

--- sim_clock/scripts/clock.py
+++ sim_clock/scripts/clock.py
@@ -13,6 +13,7 @@ import time
 from dynamic_reconfigure.server import Server
 from rosgraph_msgs.msg import Clock
 from sim_clock.cfg import ClockConfig
+from std_msgs.msg import Float32
 
 
 class SimClock():
@@ -21,11 +22,11 @@ class SimClock():
         self.cur_time = self.start_time
         self.config = None
         self.server = Server(ClockConfig, self.dr_callback)
-        self.pub = rospy.Publisher("clock", Clock, queue_size=3)
+        self.pub = rospy.Publisher("clock", Clock, queue_size=100)
+        self.sub = rospy.Subscriber("step", Float32, self.step, queue_size=10)
         while not rospy.is_shutdown():
             if self.config.play_pause:
-                self.update()
-                time.sleep(abs(self.config.dt / self.config.real_time_factor))
+                self.single_dt()
             else:
                 # wait to come out of pause
                 time.sleep(0.1)
@@ -38,17 +39,31 @@ class SimClock():
         # TODO(lucasw) need to track wall time so don't drift behind
         self.cur_time += self.config.dt
 
-    # TODO(lucasw) trigger the update from a message
+    def single_dt(self):
+        self.update()
+        time.sleep(abs(self.config.dt / self.config.real_time_factor))
+
+    # trigger the update from a message optionally or the
     # One example of a message would be from a cpu intensive node that has
     # finished processing
-    def trigger(self, msg):
-        self.update()
+    def step(self, msg):
+        # TODO(lucasw) want to keep a self.target_time around
+        # so that sequential calls to step will not round to nearest dt,
+        target_time = self.cur_time + msg.data
+        # TODO(lucasw) maybe a do-while, so one dt is always going to happen?
+        while self.cur_time < target_time and not self.config.play_pause:
+            self.single_dt()
 
     def dr_callback(self, config, level):
         self.config = config
+        # TODO(lucasw) is this potentially glitchy at the boundaries
+        # with separate thread running in init while loop?
         if not self.config.play_pause and self.config.single_step:
-            self.update()
+            self.step(Float32(config.step_time))
             self.config.single_step = False
+        elif not self.config.play_pause and self.config.single_dt:
+            self.single_dt()
+            self.config.single_dt = False
         return self.config
 
 if __name__ == '__main__':

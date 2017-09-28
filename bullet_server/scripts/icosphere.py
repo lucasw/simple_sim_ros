8ce6d69f46ef2f52d67e79e525fd2aaf473b955b
Lucas Walter <wsacul@gmail.com>
2017-09-28 07:05:19 -0700

    Now have icosahedron subdivided once, need to refactor to make it generic though.  First make a soft body with links and faces.

--- bullet_server/scripts/icosphere.py
+++ bullet_server/scripts/icosphere.py
@@ -38,6 +38,7 @@ class Icosphere:
 
         # from http://blog.andreaskahler.com/2009/06/creating-icosphere-mesh-in-code.html
         icost = (1.0 + math.sqrt(5.0)) / 2.0
+        self.radius = math.sqrt(1.0 + icost * icost)
 
         pt = Point()
         ico = []
@@ -66,37 +67,70 @@ class Icosphere:
         self.make_triangle(ico, marker.points, 0, 7, 10)
         self.make_triangle(ico, marker.points, 0, 10, 11)
 
-        self.make_triangle(ico, marker.points, 1, 5, 9)
-        self.make_triangle(ico, marker.points, 5, 11, 4)
-        self.make_triangle(ico, marker.points, 11, 10, 2)
-        self.make_triangle(ico, marker.points, 10, 7, 6)
-        self.make_triangle(ico, marker.points, 7, 1, 8)
+        if True:
+            self.make_triangle(ico, marker.points, 1, 5, 9)
+            self.make_triangle(ico, marker.points, 5, 11, 4)
+            self.make_triangle(ico, marker.points, 11, 10, 2)
+            self.make_triangle(ico, marker.points, 10, 7, 6)
+            self.make_triangle(ico, marker.points, 7, 1, 8)
 
-        self.make_triangle(ico, marker.points, 3, 9, 4)
-        self.make_triangle(ico, marker.points, 3, 4, 2)
-        self.make_triangle(ico, marker.points, 3, 2, 6)
-        self.make_triangle(ico, marker.points, 3, 6, 8)
-        self.make_triangle(ico, marker.points, 3, 8, 9)
+            self.make_triangle(ico, marker.points, 3, 9, 4)
+            self.make_triangle(ico, marker.points, 3, 4, 2)
+            self.make_triangle(ico, marker.points, 3, 2, 6)
+            self.make_triangle(ico, marker.points, 3, 6, 8)
+            self.make_triangle(ico, marker.points, 3, 8, 9)
 
-        self.make_triangle(ico, marker.points, 4, 9, 5)
-        self.make_triangle(ico, marker.points, 2, 4, 11)
-        self.make_triangle(ico, marker.points, 6, 2, 10)
-        self.make_triangle(ico, marker.points, 8, 6, 7)
-        self.make_triangle(ico, marker.points, 9, 8, 1)
+            self.make_triangle(ico, marker.points, 4, 9, 5)
+            self.make_triangle(ico, marker.points, 2, 4, 11)
+            self.make_triangle(ico, marker.points, 6, 2, 10)
+            self.make_triangle(ico, marker.points, 8, 6, 7)
+            self.make_triangle(ico, marker.points, 9, 8, 1)
 
         rospy.sleep(0.5)
         self.pub.publish(marker)
         rospy.sleep(0.5)
 
-    def make_triangle(self, src_pts, dst_pts, i0, i1, i2):
-        dst_pts.append(src_pts[i0])
-        # dst_pts.append(src_pts[i1])
+    def make_triangle(self, src, dst, i0, i1, i2, subdiv=1):
+        if subdiv == 0:
+            dst.append(src[i0])
+            # dst.append(src[i1])
 
-        dst_pts.append(src_pts[i1])
-        # dst_pts.append(src_pts[i2])
+            dst.append(src[i1])
+            # dst.append(src[i2])
 
-        dst_pts.append(src_pts[i2])
-        # dst_pts.append(src_pts[i0])
+            dst.append(src[i2])
+            # dst.append(src[i0])
+            return
+
+        dst.extend(self.sub_tri(src[i0], src[i1], src[i2]))
+
+    def mid_point(self, v1, v2):
+        pt = Point(
+                   (v1.x + v2.x) / 2.0,
+                   (v1.y + v2.y) / 2.0,
+                   (v1.z + v2.z) / 2.0)
+        # make all the points have the right radius
+        scale = self.radius / math.sqrt(pt.x * pt.x + pt.y * pt.y + pt.z * pt.z)
+        pt.x *= scale
+        pt.y *= scale
+        pt.z *= scale
+        return pt
+
+    def sub_tri(self, v1, v2, v3, levels=1):
+        #        v2
+        #
+        #     a     b
+        #  
+        #   v1   c    v3
+        a = self.mid_point(v1, v2)
+        b = self.mid_point(v2, v3)
+        c = self.mid_point(v3, v1)
+
+        return [v1, a, c,
+                c, a, b,
+                c, b, v3,
+                a, v2, b,
+                ]
 
 if __name__ == '__main__':
     rospy.init_node('icosphere')

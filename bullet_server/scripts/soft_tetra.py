d855e4305d65f6979ecda3d16ec287539dc937af
Lucas Walter <wsacul@gmail.com>
2017-10-01 06:52:35 -0700

    Having trouble making the object stay upright, maybe do need to scale everything up

--- bullet_server/scripts/soft_tetra.py
+++ bullet_server/scripts/soft_tetra.py
@@ -21,16 +21,23 @@ class SoftBodyDemo:
         ys = rospy.get_param("~y", 0.0)
         zs = rospy.get_param("~z", 1.0)
 
-        lx = rospy.get_param("~lx", 0.05)
-        ly = rospy.get_param("~ly", 0.05)
-        lz = rospy.get_param("~lz", 0.05)
+        length = rospy.get_param("~length", 0.1)
+        width = rospy.get_param("~width", 0.1)
+        height = rospy.get_param("~height", 0.1)
 
         nx = rospy.get_param("~nx", 3)
         ny = rospy.get_param("~ny", 3)
         nz = rospy.get_param("~nz", 4)
 
+        lx = length / float(nx - 1)
+        ly = width / float(ny - 1)
+        lz = height / float(nz - 1)
+        volume = lx * ly * lz
+
         total_mass = rospy.get_param("~total_mass", 0.5)
-        node_mass = total_mass / float((nx - 1) * (ny - 1) * (nz - 1))
+        num_cubes = (nx - 1) * (ny - 1) * (nz - 1)
+        tetras_per_cube = 5
+        node_mass = total_mass / float(num_cubes * tetras_per_cube)
         if True:
             body = make_soft_tetra_cube("soft_tetra_cube",
                                         node_mass,
@@ -55,13 +62,14 @@ class SoftBodyDemo:
         body.material[0].bending_distance = 2
         body.randomize_constraints = False  # True
         body.k_clusters = 4
-        # volume preserving?
-        body.config.kVC = 100.0
+        # volume preserving?  Doesn't seem to work
+        body.config.kVC = 1e9 * volume
         body.config.kDP = 0.9
-        body.config.kDF = 0.9
+        body.config.kDF = 0.999
+        # body.config.kMT = 0.5
         # pressure preserving?
         # setting this to anything with no faces (?) will result in nans
-        body.config.kPR = 0.25
+        body.config.kPR = 90000.0 * lx * ly * lz
         print body
 
         add_compound_request.soft_body.append(body)
